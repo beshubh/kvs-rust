b36ba4a31001fa86dc31c5e716c0672a05d3590f
beshubh <be.shubh42@gmail.com>
2024-11-27 23:53:37 +0530

    pass all except engine cli tests in project3

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,4 +1,5 @@
-use log::{error, info};
+use env_logger::Builder;
+use log::{error, info, LevelFilter};
 use std::{
     env,
     io::{self, BufReader, Read},
@@ -27,15 +28,14 @@ struct Cli {
 fn handle_client(mut stream: TcpStream, store: &mut KvStore) -> io::Result<()> {
     let mut reader = BufReader::new(stream.try_clone()?);
     loop {
-        let mut buf: Vec<u8> = Vec::new();
-        match reader.read_to_end(&mut buf) {
+        let mut buf: Vec<u8> = vec![0; 1024];
+        match reader.read(&mut buf) {
             Ok(0) => {
                 info!("connection closed");
                 break;
             }
             Ok(size) => {
                 let s = std::str::from_utf8(&buf[..size]).unwrap();
-                info!("message from client: {}", s);
                 let resp = common::parse_resp(s).unwrap().1;
                 let command = common::parse_command(&resp).unwrap();
                 server::handle_command(&command, &mut stream, store).unwrap();
@@ -49,13 +49,30 @@ fn handle_client(mut stream: TcpStream, store: &mut KvStore) -> io::Result<()> {
     Ok(())
 }
 
+fn handle_command(cmd: &server::Command) {
+    match cmd {
+        server::Command::Version => {
+            println!("{}", env!("CARGO_PKG_VERSION"))
+        }
+    }
+}
+
 fn main() -> Result<()> {
     dotenv::dotenv().ok();
-    env_logger::init();
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .write_style(env_logger::WriteStyle::Always)
+        .target(env_logger::Target::Stderr)
+        .init();
     let cli = Cli::parse();
+    if let Some(cmd) = &cli.cmd {
+        handle_command(cmd);
+    }
     let addr = common::parse_address(cli.address.unwrap())?;
+    let engine = cli.engine.unwrap();
     info!("{}", env!("CARGO_PKG_VERSION"));
     info!("{}", addr);
+    info!("{}", engine);
     let listener = TcpListener::bind(&addr);
     if let Err(e) = &listener {
         error!("could not bind to address: {}, error: {}", addr, e);
