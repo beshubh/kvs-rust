382b41fc01ecaacb562283cc06af4810184dff4e
beshubh <be.shubh42@gmail.com>
2024-12-01 18:43:03 +0530

    project 4, abstract our the thread pool implmentation

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -2,18 +2,27 @@ use clap::{Parser, ValueEnum};
 use env_logger::Builder;
 use kvs::engines::SledStore;
 use kvs::server::{self, KvsServer};
+use kvs::thread_pool::{NaiveThreadPool, RayonThreadPool, SharedQueueThreadPool, ThreadPool};
 use kvs::Result;
 use kvs::{KvStore, KvsEngine};
-use log::{error, info, LevelFilter};
+use log::{info, LevelFilter};
+use std::env;
 use std::env::current_dir;
 use std::net::SocketAddr;
-use std::{env, net::TcpListener};
 
 #[derive(Debug, Clone, ValueEnum)]
 #[value(rename_all = "lowercase")]
 enum Engine {
     Kvs,
     Sled,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+#[value(rename_all = "lowercase")]
+enum Pool {
+    Naive,
+    Rayon,
+    SharedQueue,
 }
 
 #[derive(Parser, Debug, Clone)]
@@ -28,6 +37,8 @@ struct Opt {
     address: SocketAddr,
     #[arg(long = "engine", global = true, value_enum ,default_value_t = Engine::Kvs)]
     engine: Engine,
+    #[arg(long = "pool", global = true, value_enum, default_value_t = Pool::SharedQueue)]
+    pool: Pool,
 }
 
 fn handle_command(cmd: &server::Command) {
@@ -61,14 +72,47 @@ fn run(opt: &Opt) -> Result<()> {
     info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
     info!("Listening on: {}", addr);
     info!("Storage engine: {:?}", engine);
-    match opt.engine {
-        Engine::Kvs => run_with_engine(KvStore::open(&current_dir()?)?, addr),
-        Engine::Sled => run_with_engine(SledStore::open(&current_dir()?)?, addr),
+
+    match (&opt.engine, &opt.pool) {
+        (Engine::Kvs, Pool::Naive) => run_with_engine(
+            KvStore::open(&current_dir()?)?,
+            NaiveThreadPool::new(1)?,
+            addr,
+        ),
+        (Engine::Kvs, Pool::Rayon) => run_with_engine(
+            KvStore::open(&current_dir()?)?,
+            RayonThreadPool::new(1)?,
+            addr,
+        ),
+        (Engine::Kvs, Pool::SharedQueue) => run_with_engine(
+            KvStore::open(&current_dir()?)?,
+            SharedQueueThreadPool::new(1)?,
+            addr,
+        ),
+        (Engine::Sled, Pool::Naive) => run_with_engine(
+            SledStore::open(&current_dir()?)?,
+            NaiveThreadPool::new(1)?,
+            addr,
+        ),
+        (Engine::Sled, Pool::Rayon) => run_with_engine(
+            SledStore::open(&current_dir()?)?,
+            RayonThreadPool::new(1)?,
+            addr,
+        ),
+        (Engine::Sled, Pool::SharedQueue) => run_with_engine(
+            SledStore::open(&current_dir()?)?,
+            SharedQueueThreadPool::new(1)?,
+            addr,
+        ),
     }
 }
 
-fn run_with_engine<E: KvsEngine>(engine: E, addr: SocketAddr) -> Result<()> {
-    let mut server = KvsServer::new(engine);
+fn run_with_engine<E: KvsEngine, P: ThreadPool>(
+    engine: E,
+    pool: P,
+    addr: SocketAddr,
+) -> Result<()> {
+    let mut server = KvsServer::new(engine, pool);
     server.run(addr)?;
     Ok(())
 }
